34fd7381f83608f0faf9feb3133de0d551d2d4c7
Anindra123 <bivasdas911@gmail.com>
2023-11-03 15:09:04 +0600

    final deployment

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,17 +5,17 @@ import GalleryItem from "./GalleryItem";
 
 //initialize the image data
 let initData = [
-  { id: 1, src: "assets/image-1.webp" },
-  { id: 2, src: "assets/image-2.webp" },
-  { id: 3, src: "assets/image-3.webp" },
-  { id: 4, src: "assets/image-4.webp" },
-  { id: 5, src: "assets/image-5.webp" },
-  { id: 6, src: "assets/image-6.webp" },
-  { id: 7, src: "assets/image-7.webp" },
-  { id: 8, src: "assets/image-8.webp" },
-  { id: 9, src: "assets/image-9.webp" },
-  { id: 10, src: "assets/image-10.jpeg" },
-  { id: 11, src: "assets/image-11.jpeg" },
+  { id: 1, src: "src/assets/image-1.webp" },
+  { id: 2, src: "src/assets/image-2.webp" },
+  { id: 3, src: "src/assets/image-3.webp" },
+  { id: 4, src: "src/assets/image-4.webp" },
+  { id: 5, src: "src/assets/image-5.webp" },
+  { id: 6, src: "src/assets/image-6.webp" },
+  { id: 7, src: "src/assets/image-7.webp" },
+  { id: 8, src: "src/assets/image-8.webp" },
+  { id: 9, src: "src/assets/image-9.webp" },
+  { id: 10, src: "src/assets/image-10.jpeg" },
+  { id: 11, src: "src/assets/image-11.jpeg" },
 ];
 
 //Main gallery component
